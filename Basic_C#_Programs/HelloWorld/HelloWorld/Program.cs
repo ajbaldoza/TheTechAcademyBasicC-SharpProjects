3c70cd47c12ad51689f4c8f7bfab3d931f574f57
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-05-17 21:56:28 -0700

    Variables and data

--- Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello World!");
         Console.ReadLine();
         }
     }
---
+++ Basic_C#_Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightsInCentimeters = 211.30123021;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Jesse";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+        }
+    }
+}
