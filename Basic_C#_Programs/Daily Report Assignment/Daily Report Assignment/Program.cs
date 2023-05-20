965852985c3228a9ae55e048cd91da81dc391ca2
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-05-20 12:05:01 -0700

    Daily report assignment

---
+++ Basic_C#_Programs/Daily Report Assignment/Daily Report Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Daily_Report_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Academy of Learning Career College");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please anser \"True\" or \"False\".");
+            string needHelp = Console.ReadLine();
+            bool needHelpBool = bool.Parse(needHelp);
+            Console.WriteLine("Were there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudy = Console.ReadLine();
+            int hoursStudyNum = Convert.ToInt32(hoursStudy);
+            Console.WriteLine("Thank you for your ansers. An instructor will respond to this shortly. have a great day!");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
