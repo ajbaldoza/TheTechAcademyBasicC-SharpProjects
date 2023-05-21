e494e17b6cdafed7f6d7e63e6e9daa84d7d945a7
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-05-20 17:40:20 -0700

    BranchingAssignment

---
+++ Basic_C#_Programs/Branching Assignment/Branching Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Branching_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. please follow the instructions below");
+            Console.WriteLine("What is the weight of your package?");
+            int weight = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.Write("Please enter the weight of the package in pounds: ");
+            decimal weight = decimal.Parse(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return; // Exit the program
+            }
+
+            Console.Write("Please enter the width of the package in inches: ");
+            decimal packagewidth = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the height of the package in inches: ");
+            decimal packageheight = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the length of the package in inches: ");
+            decimal packagelength = decimal.Parse(Console.ReadLine());
+
+          
+
+            if (packagewidth > 50)
+
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return; // Exit the program
+            }
+
+            if (packageheight > 50)
+
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return; // Exit the program
+            }
+
+            if (packagelength > 50)
+
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return; // Exit the program
+            }
+
+            decimal volume = packagewidth * packageheight * packagelength;
+
+            decimal quote = volume * weight;
+
+            decimal finalQuote = quote / 100;
+
+            Console.WriteLine("Your estimated total for shipping this package is:" + finalQuote.ToString("$0.00"));
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
