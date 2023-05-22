a824f395430c4f07c369dc45898ea9b064afb10f
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-05-21 21:16:24 -0700

    Loops

---
+++ Basic_C#_Programs/Loops/Loops/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You're wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+
+            }
+
+            while (!isGuessed);
+          
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
