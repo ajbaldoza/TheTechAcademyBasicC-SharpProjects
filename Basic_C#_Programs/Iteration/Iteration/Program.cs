8708a3ec2b69d36151f26e9bf0f6a0969092041f
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-05-26 20:35:59 -0700

    ExceptionHandling

---
+++ Basic_C#_Programs/Iteration/Iteration/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int[] testscores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+        //for (int i = 0; i < testscores.Length; i++)
+        //{
+        //    if (testscores[i] > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + testscores[i]);
+        //    }
+        string[] names = { "AJ", "JC", "KK", "CC" };
+
+        for (int j = 0; j < names.Length; j++)
+        {
+            if (names[j] == "AJ")
+            {
+                Console.WriteLine(names[j]);
+            }
+
+        }
+
+        
+        Console.ReadLine();
+    }
+}
