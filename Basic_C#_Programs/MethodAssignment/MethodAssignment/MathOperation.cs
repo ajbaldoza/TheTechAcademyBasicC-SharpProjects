bda396dba566d6e8088134c38a925f7d94600aea
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-05-26 22:12:54 -0700

    Method Assignment

---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment/MathOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    // Create a class named MathOperations
+    class MathOperations
+    {
+        // Create a void method named PerformMathOperation that takes two integers as parameters
+        public void PerformMathOperation(int firstNumber, int secondNumber)
+        {
+            // Perform a math operation on the first integer (e.g., multiply by 2)
+            int result = firstNumber * 2;
+
+            // Display the second integer to the screen
+            Console.WriteLine("Second Number: " + secondNumber);
+        }
+    }
+
+}
---
+++ Basic_C#_Programs/MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Create a class named MathOperations
+class MathOperations
+{
+    // Create a void method named PerformMathOperation that takes two integers as parameters
+    public void PerformMathOperation(int firstNumber, int secondNumber)
+    {
+        // Perform a math operation on the first integer (e.g., multiply by 2)
+        int result = firstNumber * 2;
+
+        // Display the second integer to the screen
+        Console.WriteLine("Second Number: " + secondNumber);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate the MathOperations class
+        MathOperations mathObj = new MathOperations();
+
+        // Call the PerformMathOperation method in the class, passing in two numbers (5 and 10)
+        mathObj.PerformMathOperation(5, 10);
+
+        // Call the PerformMathOperation method in the class, specifying the parameters by name
+        mathObj.PerformMathOperation(firstNumber: 3, secondNumber: 7);
+    }
+
+}
