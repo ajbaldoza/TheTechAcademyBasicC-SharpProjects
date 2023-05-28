054bbc33b8c357774c0ecd72306d6fc1edce465c
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-05-27 20:24:09 -0700

    updates

--- Basic_C#_Programs/ClassesAndObjects/ClassesAndObjects/Program.cs
+++ Basic_C#_Programs/ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -11,36 +11,21 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
+            game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
             game.ListPlayers();
-            game.Play();
             Console.ReadLine();
 
-        //    Deck deck = new Deck();
-        //    deck = Shuffle(deck);
+            //    Deck deck = new Deck();
+            //    deck.Shuffle(3);
 
-        //    foreach (Card card in deck.Cards)
-        //    {
-        //        Console.WriteLine(card.Face + " of " + card.Suit);
-        //    }
-        //    Console.WriteLine(deck.Cards.Count);
-        //    Console.Read();
-        //}
-
-        //public static Deck Shuffle(Deck deck)
-        //{
-        //    List<Card> TempList = new List<Card>();
-        //    Random random = new Random();
-
-        //    while (deck.Cards.Count > 0)
-        //    {
-        //        int randomIndex = random.Next(0, deck.Cards.Count);
-        //        TempList.Add(deck.Cards[randomIndex]);
-        //        deck.Cards.RemoveAt(randomIndex);
-        //    }
-        //    deck.Cards = TempList;
-        //    return deck;
-
+            //    foreach (Card card in deck.Cards)
+            //    {
+            //        Console.WriteLine(card.Face + " of " + card.Suit);
+            //    }
+            //    Console.WriteLine(deck.Cards.Count);
+            //    Console.Read();
         }
+
+
     }
 }
---
+++ Basic_C#_Programs/MethodsandObjectsAssignment/MethodsandObjectsAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsandObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee sam = new Employee() { FirstName = " Sample ", LastName = " Student " };
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
