0fde8a85483065fe13fc9006d3faa7bf4aef9b3a
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-06-01 19:40:19 -0700

    Method Class Assignment 1

---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/MathOperations1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class MathOperations1
+    {
+        // Create a void method named PerformMathOperation that takes two integers as parameters
+        public int MathOperation(int k, int j = 1)
+        {
+            // Perform a math operation 
+            int result = k * j;
+            return result;
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations1 mathOp = new MathOperations1();
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number: ");
+            try
+            {
+                //call method with 2 parameter
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int results = mathOp.MathOperation(num1, num2));
+                Console.WriteLine(num1 + " x " + num2 + " = " + results);
+            }
+            catch
+            {
+                //call method with one parameter
+                int results = mathOp.MathOperation(num1);
+                Console.WriteLine(num1 + "x Default 1 = " + results);
+            }
+            Console.ReadLine();
+
+
+        }
+
+
+
+    }
+}
