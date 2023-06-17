0df71ec269fe9c11592a260cd3113db46e991b95
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-06-17 09:19:12 -0700

    BasicStudentDatabase assignment

---
+++ Basic_C#_Programs/BasicStudentDatabase/BasicStudentDatabase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BasicStudentDatabase
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                var student = new Student { Name = "Billy Jeans" };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+
+                Console.WriteLine("Student added successfully!");
+
+                // Retrieve all students from the database
+                var students = db.Students;
+                foreach (var s in students)
+                {
+                    Console.WriteLine($"Student ID: {s.StudentId}, Name: {s.Name}");
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
