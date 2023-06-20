a022bede2499b4358edd8aab1e42738e9152b09e
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-06-20 12:23:31 -0700

    DatabaseAsssignment

---
+++ Basic_C#_Programs/DatabaseAssignment/DatabaseAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Dave" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
