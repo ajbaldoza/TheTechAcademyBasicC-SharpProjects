2080db3821411dbe2bcc038d4f17ea082a04ee7b
Anthony Baldoza <aj.baldoza@yahoo.com>
2023-06-19 18:08:37 -0700

    Assignment

--- Basic_C#_Programs/StudentDatabase/StudentDatabase/Program.cs
+++ Basic_C#_Programs/StudentDatabase/StudentDatabase/Program.cs
@@ -10,38 +10,43 @@ namespace StudentDatabase
     public class Student
     {
         public int StudentId { get; set; }
+
         public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 
     public class StudentContext : DbContext
     {
         public DbSet<Student> Students { get; set; }
-
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            using (var db = new StudentContext())
+            using (var context = new StudentContext())
             {
-                var student = new Student { Name = "Billy Jeans" };
-                db.Students.Add(student);
-                db.SaveChanges();
+               
+                context.Database.CreateIfNotExists();
 
-
-                Console.WriteLine("Student added successfully!");
-
-                // Retrieve all students from the database
-                var students = db.Students;
-                foreach (var s in students)
+               
+                var student = new Student
                 {
-                    Console.WriteLine($"Student ID: {s.StudentId}, Name: {s.Name}");
-                }
+                    Name = "John Doe",
+                    Age = 20
+                };
+
+             
+                context.Students.Add(student);
+
+         
+                context.SaveChanges();
+
+                Console.WriteLine("Student added to the database.");
             }
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
--- Basic_C#_Programs/StudentDatabase/StudentDatabase/Properties/AssemblyInfo.cs
+++ Basic_C#_Programs/StudentDatabase/StudentDatabase/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b53d2d9b-ed51-4ea0-96b7-cba73b13bd5e")]
+[assembly: Guid("d4b8af16-9762-4414-9149-4004a585c194")]
 
 // Version information for an assembly consists of the following four values:
 //
